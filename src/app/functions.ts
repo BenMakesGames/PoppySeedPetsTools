ba714145c7219aab4e25dfbaab0681f8a44abf0a
Ben H <ben@benmakesgames.com>
2020-10-07 16:16:23 -0400

    EVERY PET SPECIES!!!!

--- src/app/functions.ts
+++ src/app/functions.ts
@@ -1,74 +1,8 @@
 import {GripModel, HatModel, SpeciesModel} from "./models";
 
 export const SPECIES: SpeciesModel[] =
-[
-  {
-    image: 'desikh',
-    flipX: false,
-    handX: 0.3125,
-    handY: 0.71875,
-    handAngle: 78,
-    handBehind: false,
-    hatX: 0.5375,
-    hatY: 0.2583333,
-    hatAngle: -5,
-  },
-  {
-    image: 'cotton-candy',
-    flipX: false,
-    handX: 0.5,
-    handY: 0.5,
-    handAngle: -85,
-    handBehind: false,
-    hatX: 0.71041,
-    hatY: 0.26875,
-    hatAngle: 10,
-  },
-  {
-    image: 'amalgam',
-    flipX: false,
-    handX: 0.53958,
-    handY: 0.81458,
-    handAngle: -60,
-    handBehind: false,
-    hatX: 0.69375,
-    hatY: 0.67916666,
-    hatAngle: 20,
-  },
-  {
-    image: 'odd-flying-thing',
-    flipX: true,
-    handX: 0.671875,
-    handY: 0.546875,
-    handAngle: 148,
-    handBehind: false,
-    hatX: 0.4083333,
-    hatY: 0.4125,
-    hatAngle: -40,
-  },
-  {
-    image: 'rainbow-dolphin',
-    flipX: true,
-    handX: 0.3416666,
-    handY: 0.2,
-    handAngle: 0,
-    handBehind: false,
-    hatX: 0.47916666,
-    hatY: 0.40625,
-    hatAngle: 75,
-  },
-  {
-    image: 'bulbun',
-    flipX: true,
-    handX: 0.32708,
-    handY: 0.7583333,
-    handAngle: -40,
-    handBehind: true,
-    hatX: 0.4875,
-    hatY: 0.425,
-    hatAngle: 0,
-  }
-];
+  [{"image":"monotreme\/desikh","flipX":false,"handX":0.3125,"handY":0.71875,"handAngle":78,"handBehind":false,"hatX":0.5375,"hatY":0.2583333,"hatAngle":-5},{"image":"mammal\/roundish","flipX":true,"handX":0.8125,"handY":0.6875,"handAngle":-5,"handBehind":false,"hatX":0.46041,"hatY":0.36875,"hatAngle":-5},{"image":"elemental\/cotton-candy","flipX":false,"handX":0.5,"handY":0.5,"handAngle":-85,"handBehind":false,"hatX":0.71041,"hatY":0.26875,"hatAngle":10},{"image":"bird\/chickie","flipX":false,"handX":0.625,"handY":0.875,"handAngle":8,"handBehind":false,"hatX":0.39791,"hatY":0.6083333,"hatAngle":-30},{"image":"mammal\/mole","flipX":true,"handX":0.28125,"handY":0.859375,"handAngle":-45,"handBehind":false,"hatX":0.45,"hatY":0.54,"hatAngle":-10},{"image":"bird\/odd-flying-thing","flipX":true,"handX":0.671875,"handY":0.546875,"handAngle":148,"handBehind":false,"hatX":0.4083333,"hatY":0.4125,"hatAngle":-40},{"image":"elemental\/triangle","flipX":false,"handX":0.203125,"handY":0.90625,"handAngle":-3,"handBehind":false,"hatX":0.516666,"hatY":0.575,"hatAngle":0},{"image":"fish\/ba-ha","flipX":true,"handX":0.125,"handY":0.725,"handAngle":-15,"handBehind":false,"hatX":0.416666,"hatY":0.4875,"hatAngle":-5},{"image":"mammal\/much-cuter-mousie","flipX":true,"handX":0.67083333,"handY":0.4416666,"handAngle":-45,"handBehind":false,"hatX":0.33541,"hatY":0.68125,"hatAngle":5},{"image":"fungus\/mushroom","flipX":false,"handX":0.4,"handY":0.84375,"handAngle":-22,"handBehind":false,"hatX":0.47916666,"hatY":0.5625,"hatAngle":-10},{"image":"mammal\/bunny","flipX":false,"handX":0.62083333,"handY":0.947916666,"handAngle":30,"handBehind":false,"hatX":0.63125,"hatY":0.59375,"hatAngle":-10},{"image":"bird\/bi-colored-peacock","flipX":true,"handX":0.3333,"handY":0.375,"handAngle":-175,"handBehind":false,"hatX":0.47916666,"hatY":0.3,"hatAngle":5},{"image":"elemental\/a-d","flipX":true,"handX":0.25416666,"handY":0.50625,"handAngle":10,"handBehind":false,"hatX":0.5125,"hatY":0.4,"hatAngle":5},{"image":"lizard\/sneqo","flipX":false,"handX":0.4083333,"handY":0.6083333,"handAngle":80,"handBehind":false,"hatX":0.52083333,"hatY":0.12,"hatAngle":-12},{"image":"mammal\/bulbun","flipX":true,"handX":0.32708,"handY":0.7583333,"handAngle":-40,"handBehind":true,"hatX":0.4875,"hatY":0.425,"hatAngle":0},{"image":"slime\/amalgam","flipX":false,"handX":0.53958,"handY":0.81458,"handAngle":-60,"handBehind":false,"hatX":0.69375,"hatY":0.67916666,"hatAngle":20},{"image":"bird\/hatoful","flipX":false,"handX":0.86041,"handY":0.7416666,"handAngle":43,"handBehind":false,"hatX":0.5083333,"hatY":0.26458,"hatAngle":-2},{"image":"bird\/short-tailed-pochard","flipX":false,"handX":0.72291,"handY":0.69375,"handAngle":30,"handBehind":true,"hatX":0.49583333,"hatY":0.22,"hatAngle":-20},{"image":"mammal\/giraffalo","flipX":true,"handX":0.225,"handY":0.6958,"handAngle":-11,"handBehind":false,"hatX":0.39375,"hatY":0.17291,"hatAngle":2},{"image":"mammal\/rainbow-dolphin","flipX":true,"handX":0.3416666,"handY":0.2,"handAngle":0,"handBehind":false,"hatX":0.47916666,"hatY":0.40625,"hatAngle":75},{"image":"mammal\/unicorn","flipX":false,"handX":0.43125,"handY":0.81875,"handAngle":75,"handBehind":false,"hatX":0.56,"hatY":0.55,"hatAngle":-1},{"image":"mammal\/star-monkey","flipX":false,"handX":0.75,"handY":0.75,"handAngle":40,"handBehind":true,"hatX":0.60416666,"hatY":0.415,"hatAngle":0},{"image":"mammal\/noombat","flipX":false,"handX":0.84583333,"handY":0.86666,"handAngle":50,"handBehind":false,"hatX":0.82,"hatY":0.66,"hatAngle":0},{"image":"cephalopod\/hexastrephidae","flipX":false,"handX":0.59375,"handY":0.83541,"handAngle":60,"handBehind":false,"hatX":0.34375,"hatY":0.4416666,"hatAngle":15},{"image":"mammal\/star-belly","flipX":true,"handX":0.69583333,"handY":0.65208,"handAngle":5,"handBehind":false,"hatX":0.46458,"hatY":0.22916666,"hatAngle":-5},{"image":"bird\/eggid","flipX":true,"handX":0.39375,"handY":0.81875,"handAngle":-40,"handBehind":false,"hatX":0.5583333,"hatY":0.49,"hatAngle":15},{"image":"elemental\/head","flipX":true,"handX":0.51875,"handY":0.95625,"handAngle":80,"handBehind":false,"hatX":0.44375,"hatY":0.5,"hatAngle":-15},{"image":"mammal\/tentacat","flipX":false,"handX":0.85,"handY":0.5916666,"handAngle":10,"handBehind":false,"hatX":0.625,"hatY":0.14375,"hatAngle":0},{"image":"mammal\/catmouse","flipX":false,"handX":0.6666,"handY":0.825,"handAngle":45,"handBehind":false,"hatX":0.51458,"hatY":0.51041,"hatAngle":0},{"image":"mammal\/pegasus","flipX":true,"handX":0.3375,"handY":0.64583333,"handAngle":-15,"handBehind":false,"hatX":0.46458,"hatY":0.35,"hatAngle":-5},{"image":"bird\/parrot","flipX":false,"handX":0.73125,"handY":0.35,"handAngle":45,"handBehind":true,"hatX":0.5916666,"hatY":0.15,"hatAngle":-10},{"image":"fish\/porkfish","flipX":true,"handX":0.5916666,"handY":0.63958,"handAngle":-30,"handBehind":false,"hatX":0.26041,"hatY":0.51041,"hatAngle":5},{"image":"plant\/tomate","flipX":false,"handX":0.26875,"handY":0.89583333,"handAngle":86,"handBehind":false,"hatX":0.54375,"hatY":0.58125,"hatAngle":8},{"image":"lizard\/lilzilla","flipX":false,"handX":0.37,"handY":0.845,"handAngle":63,"handBehind":false,"hatX":0.545,"hatY":0.22,"hatAngle":-9},{"image":"mammal\/jelephant","flipX":false,"handX":0.75,"handY":0.71,"handAngle":15,"handBehind":false,"hatX":0.48,"hatY":0.565,"hatAngle":0},{"image":"lizard\/poppy-dragon","flipX":false,"handX":0.075,"handY":0.79,"handAngle":10,"handBehind":false,"hatX":0.775,"hatY":0.725,"hatAngle":3},{"image":"cephalopod\/sentinel","flipX":true,"handX":0.91,"handY":0.375,"handAngle":26,"handBehind":true,"hatX":0.105,"hatY":0.005,"hatAngle":-123},{"image":"bug\/novendecipede","flipX":false,"handX":0.89,"handY":0.735,"handAngle":22,"handBehind":false,"hatX":0.78,"hatY":0.365,"hatAngle":-12},{"image":"machine\/grabber","flipX":false,"handX":0.74,"handY":0.21,"handAngle":27,"handBehind":false,"hatX":0.38,"hatY":0.545,"hatAngle":-49},{"image":"bug\/sentient-beetle","flipX":false,"handX":0.865,"handY":0.63,"handAngle":12,"handBehind":false,"hatX":0.665,"hatY":0.5,"hatAngle":-20},{"image":"mammal\/hedgehog","flipX":false,"handX":0,"handY":0.95,"handAngle":106,"handBehind":false,"hatX":0.805,"hatY":0.73,"hatAngle":7},{"image":"gastropod\/just-a-snail","flipX":false,"handX":0.815,"handY":0.9,"handAngle":61,"handBehind":false,"hatX":0.46,"hatY":0.465,"hatAngle":1},{"image":"plant\/pear","flipX":false,"handX":0.405,"handY":0.865,"handAngle":51,"handBehind":false,"hatX":0.585,"hatY":0.265,"hatAngle":0},{"image":"lizard\/painter-gecko","flipX":false,"handX":0.79,"handY":0.725,"handAngle":32,"handBehind":false,"hatX":0.675,"hatY":0.45,"hatAngle":-15},{"image":"monotreme\/wibblet","flipX":false,"handX":0.49,"handY":0.665,"handAngle":80,"handBehind":false,"hatX":0.52,"hatY":0.435,"hatAngle":2},{"image":"mammal\/eee-piggie","flipX":true,"handX":0.24,"handY":0.76,"handAngle":-58,"handBehind":true,"hatX":0.35,"hatY":0.59,"hatAngle":15},{"image":"bird\/mochabo","flipX":true,"handX":0.145,"handY":0.36,"handAngle":-176,"handBehind":false,"hatX":0.35,"hatY":0.31,"hatAngle":58},{"image":"mammal\/pvirla","flipX":false,"handX":0.85,"handY":0.645,"handAngle":19,"handBehind":false,"hatX":0.45,"hatY":0.415,"hatAngle":-8},{"image":"slime\/flaand","flipX":false,"handX":0.205,"handY":0.655,"handAngle":9,"handBehind":false,"hatX":0.475,"hatY":0.63,"hatAngle":-7},{"image":"mammal\/alot","flipX":false,"handX":0.75,"handY":0.78,"handAngle":27,"handBehind":false,"hatX":0.475,"hatY":0.475,"hatAngle":0},{"image":"elemental\/earthkin","flipX":false,"handX":0.275,"handY":0.9,"handAngle":80,"handBehind":false,"hatX":0.49,"hatY":0.45,"hatAngle":-1},{"image":"mammal\/salad","flipX":false,"handX":0.755,"handY":0.705,"handAngle":63,"handBehind":true,"hatX":0.705,"hatY":0.41,"hatAngle":0}]
+;
 
 export function calculateToolStyles(size: string, species: SpeciesModel, grip: GripModel)
 {
